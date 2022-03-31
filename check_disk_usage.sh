b0274b5a1e46d8b12b281a5e47dfad5b41158c7c
1709mrd <1709mrd@gmail.com>
2022-03-31 15:27:40 +0000

    wrong commit

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -3,6 +3,6 @@ dir="$1"
 if [ -d $dir ]; then 
    echo -e "Checking disk usage in $dir \n"
 else
-   echo "$dir does not exist directory, exiting."
+   echo "$dir doees not exist directory, exiting."
    exit 101
 fi
